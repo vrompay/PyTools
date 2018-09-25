dfd94f9067545e8da64dcf2d800e2585d3001316
vrompay <hansjorgvanrompay@gmail.com>
2018-09-25 14:37:28 +0200

    Html start-up

--- html.py
+++ html.py
@@ -1,1 +1,26 @@
 #build html helper
+file = open("output/" + input("Enter file name: "),"w")
+
+file.write("<html><head>")
+
+# Title
+title = ""
+title = input("Enter title (blank for none): ")
+
+if title != "":
+	file.write("<title>" + title + "</title>")
+
+# Author
+author = ""
+author = input("Enter author (blank for none): ")
+
+if author != "":
+	file.write("<meta name=\"author\" content=\"" + author + "\""">")
+
+file.write("</head><body>")
+
+file.write("</body></html>")
+
+print(file)
+
+file.close()
