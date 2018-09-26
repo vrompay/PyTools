478d5b06ab9076696cd4f78bc6997aa96520dd60
vrompay <hansjorgvanrompay@gmail.com>
2018-09-26 15:15:15 +0200

    Added menu option to html maker

--- programs/Html.py
+++ programs/Html.py
@@ -10,10 +10,10 @@ print("######################HTML BUILDER####################")
 print("######################################################\n\n")
 
 # Start index
-html = input("Enter file name (no extentions): ") + ".html"
-file = open("./output/" + html,"w")
+html = input("Enter file name: ") + ".html"
+file = open("output/" + html ,"w")
 
-file.write("<html>\n\t<head>\n")
+file.write("<html>\n\t<head>")
 
 # Title
 title = ""
@@ -39,33 +39,49 @@ if css != "":
 	create = ""
 	create = input("Create stylesheet? (Y/N): ").lower()
 	if create == "y":
-		style = open("./output/" + css, "w")
-		style.write("* {")
-		style.write("\n\tpadding: 0;")
-		style.write("\n\tmarging: 0;")
-		style.write("\n\tfont-family: Arial, sans-serif;")
-		style.write("\n}")
+		style = open("output/" + css, "w")
+		lines = ["* {", "\n\tpadding: 0;","\n\tmarging: 0;","\n\tfont-family: Arial, sans-serif;", "\n}"]
+		for line in lines:
+			style.write(line)
 
 # Writing CSS
-if input("Build CSS? (Y/N): "):
-	backgroundColour = ""
-	backgroundColour = input("Choose background colour (blank for none): ")
-	if backgroundColour != "":
-		style.write("\n")
-		style.write("\nbody {")
-		style.write("\n\tbackground-color: " + backgroundColour + ";")
-		style.write("\n}")
+if css != "":
+	if input("Build CSS? (Y/N): "):
+		backgroundColour = ""
+		backgroundColour = input("Choose background colour (blank for none): ")
+		if backgroundColour != "":
+			style.write("\n")
+			style.write("\nbody {")
+			style.write("\n\tbackground-color: " + backgroundColour + ";")
+			style.write("\n}")
 
 # Stop head, start body
-file.write("\n\t\t</head>\n\t\t<body>")
+file.write("\n\t</head>\n\t<body>")
+
+# Menu
+list = []
+option = "Menu options here"
+
+while option != "":
+	option = input("Add menu option (enter to stop): ")
+	if option != "":
+		list.append(option)
+
+if list != []:
+	file.write("\n\t\t<ul>")
+	print("Please add the extention of the files you link")
+	for item in list:
+		file.write("\n\t\t\t<li><a href=\"" + input("Link \"" + item + "\" with: ")  +  "\">" + item + "</a></li>")
+	file.write("\n\t\t</ul>")
+
 
 # Stop body, stop html
-file.write("\n\t\t</body>\n</html>")
-
-
+file.write("\n\t</body>\n</html>")
 
 # Closing files
-style.close()
+if css != "":
+	style.close()
+
 file.close()
 
 # Clearing screen
@@ -73,7 +89,7 @@ os.system("clear")
 
 # Reopening HTML file, printing and closing
 print("HTML FILE:\n\n")
-read = open("./output/" + html)
+read = open("output/" + html)
 print(read.read())
 read.close()
 
@@ -83,11 +99,12 @@ input("\n\n\nEnter to continue")
 # Clearing screen
 os.system("clear")
 
+if css != "":
 # Reopening CSS file, printing and closing
-print("CSS FILE:\n\n")
-readcss = open("./output/" + css)
-print(readcss.read())
-readcss.close()
+	print("CSS FILE:\n\n")
+	readcss = open("output/" + css)
+	print(readcss.read())
+	readcss.close()
 
 # Enter to close
 input("\n\n\nEnter to close, you can find your files in the output folder.")
