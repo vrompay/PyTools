cfa975f57f4dce61521bcfb30d8eae2a701c7257
vrompay <hansjorgvanrompay@gmail.com>
2018-09-26 09:27:26 +0200

    Added css styling in html maker

--- html.py
+++ html.py
@@ -1,25 +1,42 @@
 #build html helper
-file = open("output/" + input("Enter file name: "),"w")
+html = input("Enter file name: ")
+file = open("output/" + html,"w")
 
-file.write("<html><head>")
+file.write("<html>\n\t<head>\n")
 
 # Title
 title = ""
 title = input("Enter title (blank for none): ")
 
 if title != "":
-	file.write("<title>" + title + "</title>")
+	file.write("\n\t\t<title>" + title + "</title>")
 
 # Author
 author = ""
 author = input("Enter author (blank for none): ")
 
 if author != "":
-	file.write("<meta name=\"author\" content=\"" + author + "\""">")
+	file.write("\n\t\t<meta name=\"author\" content=\"" + author + "\""">")
 
-file.write("</head><body>")
+# CSS
+css = ""
+css = input("Enter stylesheet name (blank for none): ")
 
-file.write("</body></html>")
+if css != "":
+	file.write("\n\t\t<link rel=\"stylesheet\" type=\"text/css\" href=\"" + css + "\">")
+	create = ""
+	create = input("Create stylesheet? (Y/N): ")
+	if create != "":
+		style = open("output/" + css, "w")
+		style.write("* {")
+		style.write("\n\tpadding: 0;")
+		style.write("\n\tmarging: 0;")
+		style.write("\n}")
+		style.close()
+
+file.write("\n\t\t</head>\n\t\t<body>")
+
+file.write("\n\t\t</body>\n</html>")
 
 print(file)
 
